79c4ebeb9fe1b50dd7a4ddc23a154bab206f8919
Edberaga <edbetjonnathan@gmail.com>
2023-06-13 17:24:57 +0700

    commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuEy2g93cjuWnd4fycwx162QxDC7KeALA",
+  authDomain: "thaigood4u-server.firebaseapp.com",
+  projectId: "thaigood4u-server",
+  storageBucket: "thaigood4u-server.appspot.com",
+  messagingSenderId: "169038433960",
+  appId: "1:169038433960:web:ffcc87715079df7aabbc9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
