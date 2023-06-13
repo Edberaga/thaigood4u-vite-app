b92314e3d2e404bb2019472408d159e5d58c0252
Edberaga <edbetjonnathan@gmail.com>
2023-06-13 19:02:40 +0700

    Develop header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="App-header" style={{textAlign: "center"}}>
+        <h2>Thaigoo4u Affliate Program</h2>
+    </header>
+  )
+}
+
+export default Header
