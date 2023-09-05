6531891d54e36cfb36c2023ca071e050314c8632
Edberaga <edbetjonnathan@gmail.com>
2023-09-05 22:46:54 +0700

    Remove the default list for a while

--- src/components/Affliate/ListData.jsx
+++ src/components/Affliate/ListData.jsx
@@ -4,100 +4,12 @@ import { useState } from 'react';
 import DefaultListData from '../AffliateDefault/DefaultListData';
 
 
-const treeData = [
-  {
-    title: 'Jackson034 (JACKSONTHAI9062023)',
-    key: '0-0',
-    icon: <CarryOutOutlined />,
-    children: [
-      {
-        title: 'Ana chan (ANATHAI27062023)',
-        key: '0-0-0',
-        icon: <CarryOutOutlined />,
-        children: [
-          {
-            title: 'David (DavidI11102023)',
-            key: '0-0-0-0',
-            icon: <CarryOutOutlined />,
-          },
-          {
-            title: (
-              <>
-                <div>Thomson (THOMSON12302023)</div>
-              </>
-            ),
-            key: '0-0-0-1',
-            icon: <CarryOutOutlined />,
-          },
-          {
-            title: 'Jerry (JERRY12302023)',
-            key: '0-0-0-2',
-            icon: <CarryOutOutlined />,
-          },
-        ],
-      },
-      {
-        title: 'Thomas(THOMSON12302023)',
-        key: '0-0-1',
-        icon: <CarryOutOutlined />,
-        children: [
-          {
-            title: 'leaf',
-            key: '0-0-1-0',
-            icon: <CarryOutOutlined />,
-          },
-        ],
-      },
-      {
-        title: 'Tony (THOMSON12302023)',
-        key: '0-0-2',
-        icon: <CarryOutOutlined />,
-        children: [
-          {
-            title: 'leaf',
-            key: '0-0-2-0',
-            icon: <CarryOutOutlined />,
-          },
-          {
-            title: 'leaf',
-            key: '0-0-2-1',
-            icon: <CarryOutOutlined />,
-            switcherIcon: <FormOutlined />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'John0118 (JOHN12302023)',
-    key: '0-1',
-    icon: <CarryOutOutlined />,
-    children: [
-      {
-        title: 'Username (USER12302023)',
-        key: '0-1-0',
-        icon: <CarryOutOutlined />,
-        children: [
-          {
-            title: 'Username (USER12302023)',
-            key: '0-1-0-0',
-            icon: <CarryOutOutlined />,
-          },
-          {
-            title: 'Username (USER12302023)',
-            key: '0-1-0-1',
-            icon: <CarryOutOutlined />,
-          },
-        ],
-      },
-    ],
-  },
-];
 
 const ListData = () => {
   const [showLine, setShowLine] = useState(true);
   const [showIcon, setShowIcon] = useState(false);
   const [showLeafIcon, setShowLeafIcon] = useState(true);
+
   const onSelect = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
   };
