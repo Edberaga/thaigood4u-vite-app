8568c00ab16c5a77bdf5de908fdbc1f8e362365a
Edberaga <edbetjonnathan@gmail.com>
2023-09-05 22:46:39 +0700

    Make it require the auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import ListData from './components/Affliate/ListData'
 import { Navigate, Route, Router, Routes } from 'react-router-dom'
-
-
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Login from './components/login';
@@ -14,24 +12,25 @@ import DefaultListData from './components/AffliateDefault/DefaultListData';
 function App() {
   const [user] = useAuthState(auth);
 
-  {/*const RequireAuth = ({children}) => {
+  const RequireAuth = ({children}) => {
     return user ? (children) : <Navigate to="/login"/>
-  };*/}
+  };
 
   return (
-    <>
-    <Header/>
-    <main className="App">
-        <Routes>
-          <Route path='/login' element={<Login/>}/>
-          <Route >
-            <Route path='/' element={<ListData/>}/>
-            <Route path='/default' element={<DefaultListData/>}/>
-            <Route path='/register' element={<Register/>}/>
-          </Route>
-        </Routes>
-    </main>
-    </>
+  <>
+  <Header/>
+  <main className="App">
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      {/*<Route element={<RequireAuth/>}> */}
+      <Route>
+        <Route path='/' element={<RequireAuth><DefaultListData/></RequireAuth>}/>
+        <Route path='/default' element={<RequireAuth><DefaultListData/></RequireAuth> }/>
+        <Route path='/register' element={<RequireAuth><Register/></RequireAuth>}/>
+      </Route>
+    </Routes>
+  </main>
+  </>
   )
 }
 
