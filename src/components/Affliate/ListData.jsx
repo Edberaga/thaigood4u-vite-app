6f7ef3832df02407ba4ed38097824af483962047
Edberaga <edbetjonnathan@gmail.com>
2023-09-11 23:41:35 +0700

    Add the list data

--- src/components/Affliate/ListData.jsx
+++ src/components/Affliate/ListData.jsx
@@ -3,29 +3,60 @@ import { Select, Switch, Tree } from 'antd';
 import { useState } from 'react';
 import DefaultListData from '../AffliateDefault/DefaultListData';
 
+let myEmail = 'jackson@gmail.com';
 
+let docArray = [
+  {
+    code: 'THAIJACKSON01',
+    email: 'jackson@gmail.com',
+    inviterCode: '',
+    name: 'Jackson',
+  },
+  {
+    code: 'THAIDAVID01',
+    email: 'david@gmail.com',
+    inviterCode: 'THAIANA01',
+    name: 'David',
+  },
+  {
+    code: 'THAIANA01',
+    email: 'ana@gmail.com',
+    inviterCode: 'THAIJACKSON01',
+    name: 'Ana',
+  },
+  {
+    code: 'THAIJERRY01',
+    email: 'jerry@gmail.com',
+    inviterCode: 'THAIANA01',
+    name: 'Jerry',
+  },
+];
+
+const buildTree = (theEmail, theKey, docArray) => {
+  let myNode = docArray.find(d => d.email === theEmail);
+  if (!myNode) {
+    throw new Error('No doc found for email:', theEmail);
+  }
+
+  let myCode = myNode.code;
+  let myChildren = docArray.filter(d => d.inviterCode === myCode);
+
+  myNode.children = myChildren.map((child) =>
+    buildTree(child.email, theKey + '-0', docArray)
+  );
+
+  return myNode;
+};
+
+let orderedTreeData = buildTree(myEmail, '0-0', docArray);
+
+console.log(orderedTreeData);
 
 const ListData = () => {
-  const [showLine, setShowLine] = useState(true);
-  const [showIcon, setShowIcon] = useState(false);
-  const [showLeafIcon, setShowLeafIcon] = useState(true);
-
-  const onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-  };
+  
   return (
     <div>
-      <Tree
-        showLine={true}
-        showIcon={showIcon}
-        defaultExpandedKeys={['0-0-0']}
-        onSelect={onSelect}
-        treeData={treeData}
-      />
-      <h1>
-        Default: 
-      </h1>
-      <DefaultListData/>
+      
     </div>
   );
 };
