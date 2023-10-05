9f8ecdfe77f8c52815acc82e28ecd5c156f706fc
Edberaga <edbetjonnathan@gmail.com>
2023-10-05 23:26:36 +0700

    still not work. but processed!

--- src/components/Affliate/ListData.jsx
+++ src/components/Affliate/ListData.jsx
@@ -1,7 +1,6 @@
-import { CarryOutOutlined, CheckOutlined, FormOutlined } from '@ant-design/icons';
-import { Select, Switch, Tree } from 'antd';
+import { CarryOutOutlined, FormOutlined } from '@ant-design/icons';
+import { Tree } from 'antd';
 import { useState } from 'react';
-import DefaultListData from '../AffliateDefault/DefaultListData';
 
 let myEmail = 'jackson@gmail.com';
 
@@ -32,32 +31,55 @@ let docArray = [
   },
 ];
 
-const BuildTree = (theEmail, theKey, docArray) => {
+var newNode = [];
+
+const BuildNewTree = (theEmail, theKey, docArray) => {
   let myNode = docArray.find(d => d.email === theEmail);
   if (!myNode) {
     throw new Error('No doc found for email:', theEmail);
   }
 
+  newNode.push({
+    title: `${myNode.name} (${myNode.code})`,
+    key: `${theKey}`,
+    icon: <CarryOutOutlined />,
+  });
+
   let myCode = myNode.code;
   let myChildren = docArray.filter(d => d.inviterCode === myCode);
 
-  myNode.children = myChildren.map((child) =>
-    BuildTree(child.email, theKey + '-0', docArray)
+  newNode[0].children = myChildren.map((child) =>
+    BuildNewTree(child.email, theKey + '-0', docArray)
   );
 
-  return myNode;
+  return newNode;
 };
 
-let orderedTreeData = BuildTree(myEmail, '0-0', docArray);
+//let orderedTreeData = BuildTree(myEmail, '0-0', docArray);
+let newOrder = BuildNewTree(myEmail, '0-0', docArray);
 
-console.log("List: ", orderedTreeData);
+//console.log("List: ", orderedTreeData);
+console.log( newOrder);
 
 const ListData = () => {
+  const [showLine, setShowLine] = useState(true);
+  const [showIcon, setShowIcon] = useState(false);
+  const [showLeafIcon, setShowLeafIcon] = useState(true);
+  const onSelect = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  };
   
   return (
-    <>
-    
-    </>
+    <div>
+      <h2>Default Tree</h2>
+      <Tree
+        showLine={true}
+        showIcon={true}
+        defaultExpandedKeys={['0-0-0']}
+        onSelect={onSelect}
+        treeData={newOrder}
+      />
+    </div>
   );
 };
 export default ListData;
