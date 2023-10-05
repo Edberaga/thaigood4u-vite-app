9cb0f1d716b161e033ef75afba434ab5f6379b3d
Edberaga <edbetjonnathan@gmail.com>
2023-10-05 23:27:05 +0700

    Dismiss some console

--- src/components/AffliateDefault/DefaultListData.jsx
+++ src/components/AffliateDefault/DefaultListData.jsx
@@ -89,14 +89,17 @@ const defaultTreeData = [
   },
 ];
 
+
+
 const DefaultListData = () => {
-  console.log(defaultTreeData);
   const [showLine, setShowLine] = useState(true);
   const [showIcon, setShowIcon] = useState(false);
   const [showLeafIcon, setShowLeafIcon] = useState(true);
   const onSelect = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
   };
+  
+  console.log("Default: ", defaultTreeData);
   return (
     <div>
       <h2>Default Tree</h2>
