44a9f16426c09845a52192fd8e728b003865e0b6
Edberaga <edbetjonnathan@gmail.com>
2023-10-09 23:51:32 +0700

    move the build function inside the component

--- src/components/Affliate/ListData.jsx
+++ src/components/Affliate/ListData.jsx
@@ -1,9 +1,12 @@
 import { CarryOutOutlined, FormOutlined } from '@ant-design/icons';
 import { Tree } from 'antd';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth, db } from '../../firebase'
+import { doc, getDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 import './ListData.css'
 
-let myEmail = 'jackson@gmail.com';
 
 let docArray = [
   {
@@ -32,77 +35,129 @@ let docArray = [
   },
 ];
 
-const BuildNewTree = (theEmail, theKey, docArray) => {
-  let myNode = docArray.find(d => d.email === theEmail);
-  if (!myNode) {
-    throw new Error('No doc found for email:', theEmail);
-  }
-  let newNode = []
-  newNode.push({
-    title: `${myNode.name} (${myNode.code})`,
-    key: `${theKey}`,
-    icon: <CarryOutOutlined />,
-  });
+const ListData = () => {
+  const [user] = useAuthState(auth);
+  //const [docArray, setDocArray] = useState([]);
 
-  let myCode = myNode.code;
-  let myChildren = docArray.filter(d => d.inviterCode === myCode);
+  let myEmail = user.email;
+  console.log("Email: ",user.email);
 
-  //Build child, (I hope this will deleted soon...)
-  const BuildChildTree = (theEmail, theKey, docArray) => {
+  let docArray = [
+    {
+      code: 'THAIEDBERT01',
+      email: 'edbertjonnathan@gmail.com',
+      inviterCode: 'THAIKASEY01',
+      name: 'Edbert',
+    },
+    {
+      code: 'THAIDAVID01',
+      email: 'david@gmail.com',
+      inviterCode: 'THAIANA01',
+      name: 'David',
+    },
+    {
+      code: 'THAIANA01',
+      email: 'ana@gmail.com',
+      inviterCode: 'THAIJACKSON01',
+      name: 'Ana',
+    },
+    {
+      code: 'THAIJERRY01',
+      email: 'jerry@gmail.com',
+      inviterCode: 'THAIANA01',
+      name: 'Jerry',
+    },
+  ];
+  
+
+  /*
+  useEffect(() => {
+    const fetchData = async() => {
+      let list = [];
+      try{
+        const q = query(collection(db, "users"));
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((doc) => {
+          list.push({ id:doc.id, ...doc.data()});
+          console.log(doc.id, " => ", doc.data());
+        });
+        setDocArray(list);
+      }
+      catch(error){
+        console.log(error);
+      }
+    };
+    fetchData();
+  },[]);
+
+  console.log("Array: ", docArray);
+  */
+
+  const BuildNewTree = (theEmail, theKey, docArray) => {
     let myNode = docArray.find(d => d.email === theEmail);
     if (!myNode) {
       throw new Error('No doc found for email:', theEmail);
     }
-    
-    let newNode = {
+    let newNode = []
+    newNode.push({
       title: `${myNode.name} (${myNode.code})`,
       key: `${theKey}`,
       icon: <CarryOutOutlined />,
-    };
+    });
   
     let myCode = myNode.code;
     let myChildren = docArray.filter(d => d.inviterCode === myCode);
+  
+    //Build child, (I hope this will deleted soon...)
+    const BuildChildTree = (theEmail, theKey, docArray) => {
+      let myNode = docArray.find(d => d.email === theEmail);
+      if (!myNode) {
+        throw new Error('No doc found for email:', theEmail);
+      }
+      
+      let newNode = {
+        title: `${myNode.name} (${myNode.code})`,
+        key: `${theKey}`,
+        icon: <CarryOutOutlined />,
+      };
+    
+      let myCode = myNode.code;
+      let myChildren = docArray.filter(d => d.inviterCode === myCode);
+      if(!myChildren) {
+        return newNode;
+      }
+      else {
+        newNode.children = myChildren.map((child) =>
+          BuildChildTree(child.email, theKey + '-0', docArray)
+        );
+      }
+      return newNode;
+    };
+    //Build child, (I hope this will deleted soon...)
+  
     if(!myChildren) {
       return newNode;
     }
     else {
-      newNode.children = myChildren.map((child) =>
+      newNode[0].children = myChildren.map((child) =>
         BuildChildTree(child.email, theKey + '-0', docArray)
       );
     }
+  
     return newNode;
   };
-  //Build child, (I hope this will deleted soon...)
+  
+  let newOrder = BuildNewTree(myEmail, '0-0', docArray);
+  console.log( newOrder);
 
-  if(!myChildren) {
-    return newNode;
-  }
-  else {
-    newNode[0].children = myChildren.map((child) =>
-      BuildChildTree(child.email, theKey + '-0', docArray)
-    );
-  }
-
-  return newNode;
-};
-
-//let orderedTreeData = BuildTree(myEmail, '0-0', docArray);
-let newOrder = BuildNewTree(myEmail, '0-0', docArray);
-
-//console.log("List: ", orderedTreeData);
-console.log( newOrder);
-
-const ListData = () => {
-  const [showLine, setShowLine] = useState(true);
-  const [showIcon, setShowIcon] = useState(false);
-  const [showLeafIcon, setShowLeafIcon] = useState(false);
+  
   const onSelect = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
   };
   
   return (
     <div>
-      <h2>Default Tree</h2>
       <Tree
         showLine={true}
         showIcon={true}
