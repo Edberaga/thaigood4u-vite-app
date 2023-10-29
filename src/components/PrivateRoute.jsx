be8ff2baa8e3fa0bf8ca735a89a3efc775966103
Edberaga <edbetjonnathan@gmail.com>
2023-10-29 22:29:54 +0700

    Develop the Private Route

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Outlet, Navigate } from "react-router-dom";
+import Spinner from "./Spinner";
+import checkAuth from "../hooks/checkAuth";
+
+export default function PrivateRoute() {
+  const { loggedIn, checkingStatus } = checkAuth();
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+}
