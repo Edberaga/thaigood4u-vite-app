d6b1bc35e347993316d18c1dc1d47e3d306445e2
Edberaga <edbetjonnathan@gmail.com>
2023-11-09 23:56:31 +0700

    add the image

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { GetUserData } from '../../hooks/GetUserData'
+import userProfile from '../../assets/images/user-profile.png'
 
 const Dashboard = () => {
   return (
-    <section className='container'>
-        <section>
-            <section>
-                <div>
-                    <img src="" alt="User Profile Pic" />
-                </div>
-            </section>
-        </section>
-        <section></section>
+  <section className='container'>
+    <section>
+      {/*User Profile Section*/}
+      <section>
+        <div>
+            <img src={userProfile} alt="User Profile Pic" />
+        </div>
+        <div>
+          <h1>Unverified Member</h1>
+          <p>Name: <span>Edbert</span></p>
+          <p>Email: <span>edbertjonnathan@gmail.com</span></p>
+        </div>
+      </section>
     </section>
+      <section></section>
+  </section>
   )
 }
 
