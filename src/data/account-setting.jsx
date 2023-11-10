5ed2f49f046a3f72c08ab592323daa13b3670f14
Edberaga <edbetjonnathan@gmail.com>
2023-11-10 17:03:17 +0700

    Account setting data

---
+++ src/data/account-setting.jsx
@@ -0,0 +1,21 @@
+import BorderColorRoundedIcon from '@mui/icons-material/BorderColorRounded';
+import LockResetRoundedIcon from '@mui/icons-material/LockResetRounded';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+
+export const accountSettingData = [
+    {
+        name: "Edit Profile",
+        icon: <BorderColorRoundedIcon />,
+        path: '/',
+    },
+    {
+        name: "Reset Password",
+        icon: <LockResetRoundedIcon />,
+        path: '/',
+    },
+    {
+        name: "Customer Support",
+        icon: <SupportAgentIcon />,
+        path: '/',
+    },
+];
