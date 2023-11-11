ce60aacaa00e5b5e3f631b057840824bb6cb73e7
Edberaga <edbetjonnathan@gmail.com>
2023-11-12 03:24:01 +0700

    Badge data

---
+++ src/data/BadgeData.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import visit10 from '../assets/images/badges/TG4U_Badge_Activities10.png'
+import visit20 from '../assets/images/badges/TG4U_Badge_Activities20.png'
+import visit30 from '../assets/images/badges/TG4U_Badge_Activities30.png'
+import visit40 from '../assets/images/badges/TG4U_Badge_Activities40.png'
+
+const BadgeData = [
+    {
+        id: "visit10",
+        name: "Visit Our Partners 10 Times!",
+        img: visit10
+    },
+    {
+        id: "visit20",
+        name: "Visit Our Partners 20 Times!",
+        img: visit20
+    },
+    {
+        id: "visit30",
+        name: "Visit Our Partners 30 Times!",
+        img: visit30
+    },
+    {
+        id: "visit40",
+        name: "Visit Our Partners 40 Times!",
+        img: visit40
+    },
+
+]
+
+export default BadgeData
