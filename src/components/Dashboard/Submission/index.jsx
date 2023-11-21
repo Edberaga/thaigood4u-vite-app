8b607b3a57539339ebf066b7ee6bd6bbce4d2b1e
Edberaga <edbetjonnathan@gmail.com>
2023-11-21 23:43:47 +0700

    Add heading at the photo submit display

--- src/components/Dashboard/Submission/index.jsx
+++ src/components/Dashboard/Submission/index.jsx
@@ -12,49 +12,56 @@ const Submission = () => {
     <section className="form-container">
       <div className="image-display">
         <div>
-        <img 
-            src={
-            receipt ? URL.createObjectURL(receipt)
-            :"https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" }
-            alt="Insert photo"
-        />
+        <h1>Your Receipt</h1>
+          <div className='image-border'>
+          <img 
+              src={
+              receipt ? URL.createObjectURL(receipt)
+              :"https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" }
+              alt="Insert photo"
+          />
+          </div>
         </div>
+
         <div>
-        <img 
-            src={
-            selfie ? URL.createObjectURL(selfie)
-            :"https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" }
-            alt="Insert photo"
-        />
+        <h1>Your Selfie</h1>
+        <div className='image-border'>
+          <img 
+              src={
+              selfie ? URL.createObjectURL(selfie)
+              :"https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" }
+              alt="Insert photo"
+          />
+        </div>
         </div>
       </div>
 
       <form className='proof-submission' action="">
-          <label for='restaurant-name'>Restaurant Name</label>
-          <input type='text' id='travelName' required/>
+        <label for='restaurant-name'>Restaurant Name</label>
+        <input type='text' id='travelName' required/>
 
-          <label for="photoUrl">Receipt Photo</label>
-          <input 
-              type="file" 
-              id='photoUrl'
-              size="60"
-              className='input-file'
-              onChange={(e) => setReceipt(e.target.files[0])}
-              required
-          />
-          
-          <label for="selfieUrl">Selfie Photo</label>
-          <input 
-              type="file" 
-              id='selfieUrl'
-              size="60"
-              onChange={(e) => setSelfie(e.target.files[0])}
-              required
-          />
-          <button type="submit">Submit Photo</button>
+        <label for="photoUrl">Receipt Photo</label>
+        <input 
+            type="file" 
+            id='photoUrl'
+            size="60"
+            className='input-file'
+            onChange={(e) => setReceipt(e.target.files[0])}
+            required
+        />
+        
+        <label for="selfieUrl">Selfie Photo</label>
+        <input 
+            type="file" 
+            id='selfieUrl'
+            size="60"
+            onChange={(e) => setSelfie(e.target.files[0])}
+            required
+        />
+        <button type="submit">Submit Photo</button>
       </form>
     </section>
-    
+    <hr/>
     {/*Submission display */}
       <div className="display-container">
         
