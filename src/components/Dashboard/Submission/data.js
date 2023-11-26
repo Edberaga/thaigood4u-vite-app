f7e9d63214faea840f95985a07f4923345b3d83b
Edberaga <edbetjonnathan@gmail.com>
2023-11-26 23:47:10 +0700

    Temporary data.js

---
+++ src/components/Dashboard/Submission/data.js
@@ -0,0 +1,47 @@
+import receiptImg from '../../../assets/images/OIP.jpeg'
+import selfieImg from '../../../assets/images/me.jpg'
+
+export const submittedData = [
+    {
+        name: "Warkop Hau",
+        selfieImg: selfieImg,
+        receiptImg: receiptImg,
+        date: "24/07/1999",
+        status: "Submitted"
+    },
+    {
+        name: "Warung Koki",
+        selfieImg: selfieImg,
+        receiptImg: receiptImg,
+        date: "24/07/1999",
+        status: "Submitted"
+    },
+    {
+        name: "Warkop Hau",
+        selfieImg: selfieImg,
+        receiptImg: receiptImg,
+        date: "24/07/1999",
+        status: "Approved"
+    },
+    {
+        name: "Warkop Hau",
+        selfieImg: selfieImg,
+        receiptImg: receiptImg,
+        date: "24/07/1999",
+        status: "Submitted"
+    },
+    {
+        name: "Warung Koki",
+        selfieImg: selfieImg,
+        receiptImg: receiptImg,
+        date: "24/07/1999",
+        status: "Submitted"
+    },
+    {
+        name: "Warkop Hau",
+        selfieImg: selfieImg,
+        receiptImg: receiptImg,
+        date: "24/07/1999",
+        status: "Approved"
+    },
+]
